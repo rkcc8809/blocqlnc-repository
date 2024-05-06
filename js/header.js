9f732b1b1104e7b092041b0c3cf3b1ff2d613dd9
riku.sato <rkcc8809@gmail.com>
2024-05-07 03:36:24 +0900

    11th-commit

--- js/header.js
+++ js/header.js
@@ -1,17 +1,42 @@
 "use strict";
 
+// $(".h-open").click(function () {
+//   //ボタンがクリックされたら
+//   $(this).toggleClass('active');
+//   //ボタン自身に activeクラスを付与する
+//   $("#h-nav").toggleClass('panelactive');
+//   //ナビゲーションにpanelactiveクラスを付与する
+// });
+
+// $("#h-nav a").click(function () {
+//   //ナビゲーションのリンクがクリックされたら
+//   $(".h-open").removeClass('active');
+//   //ボタンの activeクラスを除去して
+//   $("#h-nav").removeClass('panelactive');
+//   //ナビゲーションのpanelactiveクラスも除去する
+// });
+
+"use strict";
+
 $(".h-open").click(function () {
-  //ボタンがクリックされたら
+  // ボタンがクリックされた時
   $(this).toggleClass('active');
-  //ボタン自身に activeクラスを付与する
+  // ボタンにactiveクラスを付与または削除する
   $("#h-nav").toggleClass('panelactive');
-  //ナビゲーションにpanelactiveクラスを付与する
+  // ナビゲーションにpanelactiveクラスを付与または削除する
 });
 
-$("#h-nav a").click(function () {
-  //ナビゲーションのリンクがクリックされたら
-  $(".h-open").removeClass('active');
-  //ボタンの activeクラスを除去して
-  $("#h-nav").removeClass('panelactive');
-  //ナビゲーションのpanelactiveクラスも除去する
+$("#h-nav a").click(function (e) {
+  // ナビゲーションのリンクがクリックされた時
+  var href = $(this).attr('href'); // 遷移先のURLを取得
+  e.preventDefault(); // 即座の遷移を防ぐ
+  setTimeout(function() {
+    window.location.href = href; // 遅延後に新しいページに遷移
+  }, 200); // 200ミリ秒の遅延（必要に応じて調整）
+
+  // クラス削除にも同様の遅延を追加する
+  setTimeout(function() {
+    $(".h-open").removeClass('active');
+    $("#h-nav").removeClass('panelactive');
+  }, 200); // クラス削除もナビゲーション遅延と同じ時間で遅延
 });
